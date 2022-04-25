c955288d8f57961b19a127e606a064b1f0e4b948
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2022-04-25 11:06:36 -0300

    module04(js-concepts-pt-2): lesson 3 - project pt2

---
+++ module-04-js-concepts-pt2/aula03-esmodules-internalization-pt2/src/index.js
@@ -0,0 +1,33 @@
+//  assert {type: "json"} -> required  to be able to import a json file (node v17)
+import database from '../database.json' assert { type: 'json' };
+
+import Person from './person.js';
+import TerminalController from './terminalController.js';
+
+const DEFAULT_LANG = 'pt-BR';
+const STOP_TERMINAL = ':q';
+
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
+
+async function mainLoop() {
+	try {
+		const answer = await terminalController.question();
+
+		if (answer === STOP_TERMINAL) {
+			terminalController.closeTerminal();
+			console.log('process finished');
+			return;
+		}
+
+		const person = Person.generateInstanceFromText(answer);
+
+		return mainLoop();
+	} catch (error) {
+		console.log('Error!!!:', error);
+		return mainLoop();
+	}
+}
+
+// top-level-await
+await mainLoop();
